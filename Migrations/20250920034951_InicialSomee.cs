34a88503db7bbfc44424a7451eb454de68a7041d
EmilioAntigua <emilioantigua48@gmail.com>
2025-09-20 13:54:10 -0400

    chore: agregar migración inicial para creación de la tabla Jugadores

---
+++ Migrations/20250920034951_InicialSomee.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicTacToeApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicialSomee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
