4fce93e5d053d5519b57ee66b3ffdf9a813ee8ff
EmilioAntigua <emilioantigua48@gmail.com>
2025-09-20 13:51:07 -0400

    feat: métodos de extensión para mostrar notificaciones de éxito, advertencia y error

---
+++ Extensors/ToastServiceExtensions.cs
@@ -0,0 +1,34 @@
+using BlazorBootstrap;
+
+namespace TicTacToeApp.Extensors;
+
+public static class ToastServiceExtensions
+{
+  
+    public static ToastMessage ShowToast(this ToastService toastService, ToastType toastType, string title, string? customMessage = null)
+    {
+        var message = new ToastMessage()
+        {
+            Type = toastType,
+            Title = title,
+            Message = customMessage ?? $"A las {DateTime.Now:hh:mm tt}"
+        };
+        toastService.Notify(message);
+        return message;
+    }
+    
+    public static ToastMessage ShowSuccess(this ToastService toastService, string? customMessage = null, string title = "Éxito")
+    {
+        return toastService.ShowToast(ToastType.Success, title, customMessage);
+    }
+
+    public static ToastMessage ShowWarning(this ToastService toastService, string? customMessage = null, string title = "Advertencia")
+    {
+        return toastService.ShowToast(ToastType.Warning, title, customMessage);
+    }
+
+    public static ToastMessage ShowError(this ToastService toastService, string? customMessage = null, string title = "Error")
+    {
+        return toastService.ShowToast(ToastType.Danger, title, customMessage);
+    }
+}
